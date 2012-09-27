aa6ec773cc0164d8ff6aac411877a1908624cd92
Tyler Dodge <tldodge@ncsu.edu>
2012-09-27 14:49:31 -0400

    Make rlv work with ugly folder names (ones with spaces)

--- rlv.sh
+++ rlv.sh
@@ -3,4 +3,4 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 TARGET=$( pwd )/$1
 echo $TARGET | grep "//" > /dev/null && TARGET=$1
-(cd $DIR; exec ./app.js  $TARGET $2)
+(cd $DIR; exec ./app.js  "$TARGET" $2)
